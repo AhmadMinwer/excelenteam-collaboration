0a91dd85fcc5c4f6c3226dbac47cccaf1232da65
Ahmad Minwer <minwer.eid.ahmad>
2020-02-05 14:55:21 +0200

    add config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    name : 'Git-Collaboration',
+    repo : 'https://github.com/AhmadMinwer/git-collaboration.git',
+    info : 'just do it.',
+    version : '1.0.0'
+} 
+
+// here is a secret message!
